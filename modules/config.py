e5aaf2d418900a353d0949abd199f04ef3c58ec1
MARCO-015 <Charlie.aimers07@gmail.com>
2025-01-18 11:12:52 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,6 +1,6 @@
 # config.py
-BOT_NAME = "jokerbhaisahab3"
-BOT_TOKEN = "7326006111:AAE-8BcoQkmyBTkqfkD2Wjc0mj1LAVKf6Gc"
-API_ID = 20299588
-API_HASH = "f550d6179131c293d658f15f8c24f594"
-MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
+BOT_NAME = "trial"
+BOT_TOKEN = "7838532960:AAF_zpM1NnSvBxTMKqJSd-KRRi2IG69H1Ao"
+API_ID = 21008992
+API_HASH = "7838532960:AAF_zpM1NnSvBxTMKqJSd-KRRi2IG69H1Ao"
+MONGO_URI = "mongodb+srv://Test2:Monstersir123@cluster0.ltadb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
