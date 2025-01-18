5dad1f5bb10c7b8268d59231b11eecb903a02957
MARCO-015 <Charlie.aimers07@gmail.com>
2025-01-18 11:09:33 +0530

    Add files via upload

---
+++ modules/config.py
@@ -0,0 +1,6 @@
+# config.py
+BOT_NAME = "jokerbhaisahab3"
+BOT_TOKEN = "7326006111:AAE-8BcoQkmyBTkqfkD2Wjc0mj1LAVKf6Gc"
+API_ID = 20299588
+API_HASH = "f550d6179131c293d658f15f8c24f594"
+MONGO_URI = "mongodb+srv://mrnobody:modernhackers@mrnobody.q8e87ij.mongodb.net/?retryWrites=true&w=majority&appName=MrNobody"
