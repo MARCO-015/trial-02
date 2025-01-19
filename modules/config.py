14455abbe9c4b235ad05f269abc457e22c48baa7
MARCO-015 <Charlie.aimers07@gmail.com>
2025-01-19 13:01:49 +0530

    Update config.py

--- modules/config.py
+++ modules/config.py
@@ -1,6 +1,6 @@
 # config.py
-BOT_NAME = "trial"
-BOT_TOKEN = "7838532960:AAF_zpM1NnSvBxTMKqJSd-KRRi2IG69H1Ao"
+BOT_NAME = "Tony"
+BOT_TOKEN = "7616665641:AAGgymSVQ06sA9boqiKMz9I15uEr8O7J_II"
 API_ID = 21008992
 API_HASH = "da87f6dea5ed8cfe1a53617e33a35742"
 MONGO_URI = "mongodb+srv://Test2:Monstersir123@cluster0.ltadb.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
